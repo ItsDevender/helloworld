aa25aa41b94c297f4e831aecf5fdd0356db4bccf
devender <itsdevtiamo@gmail.com>
2023-05-05 13:12:33 +0530

    plugins jacoco and pom update

---
+++ primary-source/src/test/java/helloworldtest.java
@@ -0,0 +1,9 @@
+public class helloworldtest {
+    public static void main(String []args)
+    {
+        System.out.println("HEllo world from Java");
+        System.out.println("Now this source code is getting edited in developer branch");
+        System.out.println("Checking another change impact on pipeline");
+        System.out.println("Checking Sonarscanner in azure pipeline. in seperate sonar branch..");
+    }
+}
